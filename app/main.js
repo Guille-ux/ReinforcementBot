c32a8fa855ef46f280710baffb3544878c233dd3
Guillermo leira temes <104579306+Guille-ux@users.noreply.github.com>
2025-03-16 17:01:13 +0000

    Delete app directory

--- app/main.js
+++
@@ -1,7 +0,0 @@
-const { app, BrowserWindow, ipcMain } = require('electron');
-const path = require("path");
-
-function window_init(w, h) {
-	const mainWindow = new BrowserWindow({width: w, height: h, webPreferences: {nodeIntegration: true, contextIsolation: false, preload: path.join(__dirname, "preload.js")}}); // crea la ventana
-	mainWindow.loadFile(path.join(__dirname))
-}
